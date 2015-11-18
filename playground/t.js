c6baaebf3fafa7f31f03d06d4c47292cd891e489
Clement Auger <clement.ager@d1m.cn>
2015-11-18 16:14:25 +0800

    make it so.

--- playground/t.js
+++ playground/t.js
@@ -1,4 +1,6 @@
 var sshot = require('..')
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 
 var app = express();
@@ -7,10 +9,12 @@ var server = app.listen(3000);
 
 sshot("http://localhost:3000/index.html", "1024*768", {})
 .on('data', function (b){
-    console.log(b+'')
+    //console.log(b+'')
   })
 .on('block', function (b){
-    console.log(b)
+    var d = path.dirname(b.file);
+    if (fs.existsSync(d)===false) fs.mkdirSync(path.dirname(b.file))
+    fs.writeFileSync(b.file, new Buffer(b.img, 'base64'))
   })
 .on('end', function (b){
     console.log('end')
